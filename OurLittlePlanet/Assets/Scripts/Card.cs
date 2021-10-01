80be01b0f619f71d865eb3953b6fdb2a74766958
Adishsh <Adishsh@users.noreply.github.com>
2021-10-01 12:55:00 +0300

    add basic text to cards

--- OurLittlePlanet/Assets/Scripts/Card.cs
+++ OurLittlePlanet/Assets/Scripts/Card.cs
@@ -5,7 +5,13 @@ using UnityEngine.UI;
 
 public class Card : MonoBehaviour
 {
-    [SerializeField] public CardData m_CardData;
+    [SerializeField] Text m_CardName;
+    [SerializeField] Text m_Description;
+    [SerializeField] Text m_Cost;
+    [SerializeField] Text m_Polution;
+    [SerializeField] Text m_Population;
+
+    public CardData m_CardData{ get; private set; }
     public bool m_isFrontUp { get; private set; }
 
     private void Start()
@@ -29,6 +35,11 @@ public class Card : MonoBehaviour
     public void SetUpCard(CardData cardData)
     {
         m_CardData = cardData;
+        m_CardName.text = cardData.m_CardName;
+        m_Description.text = cardData.m_Description;
+        m_Cost.text = cardData.m_Cost.ToString();
+        m_Polution.text = cardData.m_Pollution.ToString();
+        m_Population.text = cardData.m_Resources.ToString();
 
         Image image = gameObject.GetComponent<Image>();
         if( m_CardData.m_Sprite)
