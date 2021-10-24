f93f07ab22509f162e69559427b2564107012ca6
Adishsh <Adishsh@users.noreply.github.com>
2021-10-24 20:53:00 +0300

    eventCard - add animator and animation name

--- OurLittlePlanet/Assets/Scripts/EventCard.cs
+++ OurLittlePlanet/Assets/Scripts/EventCard.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class EventCard: MonoBehaviour
 {
+    [SerializeField] public Animator m_Animator;
+    [SerializeField] public string m_AnimationName;
+    
     public string m_EventName;
+
     public virtual void ActivateEvent(WorldMap map, StatsManager statsManager)
     {
         statsManager.SetCurrentEvent(this);
-        Debug.Log("did event!!");
+        if(!string.IsNullOrEmpty(m_AnimationName) && m_Animator != null)
+        m_Animator.Play(m_AnimationName);
     }
 }
