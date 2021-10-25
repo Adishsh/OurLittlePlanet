76338558b4a928eb1ca6656233629e05f87d2e32
ronoink <ronoink@gmail.com>
2021-10-25 17:18:37 +0300

    Camera Switch
    Switch camera angle with keys : 1, 2 , 3 , 4

---
+++ OurLittlePlanet/Assets/Scripts/CameraSwitch.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+
+    public GameObject cam1;
+    public GameObject cam2;
+    public GameObject cam3;
+    public GameObject cam4;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Key1"))
+
+        {
+            cam1.SetActive(true);
+            cam2.SetActive(false);
+            cam3.SetActive(false);
+            cam4.SetActive(false);
+        }
+        
+        if (Input.GetButtonDown("Key2"))
+
+        {
+            cam1.SetActive(false);
+            cam2.SetActive(true);
+            cam3.SetActive(false);
+            cam4.SetActive(false);
+        }
+        
+        if (Input.GetButtonDown("Key3"))
+
+        {
+            cam1.SetActive(false);
+            cam2.SetActive(false);
+            cam3.SetActive(true);
+            cam4.SetActive(false);
+        }
+        
+        
+        if (Input.GetButtonDown("Key4"))
+
+        {
+            cam1.SetActive(false);
+            cam2.SetActive(false);
+            cam3.SetActive(false);
+            cam4.SetActive(true);
+        }
+    }
+}
