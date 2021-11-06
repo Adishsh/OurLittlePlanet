d2fa88abd4733f61bcdbc201822fa0ca50b0725f
Adishsh <Adishsh@users.noreply.github.com>
2021-11-06 23:26:35 +0200

    delay the building hover

--- OurLittlePlanet/Assets/Scripts/BuildingSlot.cs
+++ OurLittlePlanet/Assets/Scripts/BuildingSlot.cs
@@ -6,16 +6,19 @@ using TMPro;
 
 public class BuildingSlot : MonoBehaviour
 {
-    [SerializeField]public int index;
-    public List<BuildingSlot> m_AdjasentSlots {get; private set;} 
-    public Building building{ get; private set;}
+    [SerializeField] public int index;
+    [SerializeField] public float m_HoveringDisplayTimee = 1f;
+    public List<BuildingSlot> m_AdjasentSlots { get; private set; }
+    public Building building { get; private set; }
     public bool WasCalculated = false;
     [SerializeField] Animator animator;
     [SerializeField] GameObject resourcesPanel;
     [SerializeField] TextMeshPro resources;
     [SerializeField] TextMeshPro polution;
+    [SerializeField] TextMeshPro Buildingname;
 
-
+    private Coroutine showingResources;
+    private bool IsHovering;
 
 
     public BuildingSlot(List<BuildingSlot> adjasentSlots)
@@ -25,26 +28,26 @@ public class BuildingSlot : MonoBehaviour
 
     public void Build(Card card, WorldMap map)
     {
-        
+
         var cardData = card.m_CardData;
         resources.text = cardData.m_Resources.ToString();
         polution.text = cardData.m_Pollution.ToString();
-
-        if( cardData.m_Building &&  cardData.m_Building.CanBuildBuilding(map))
+        Buildingname.text = cardData.m_CardName;
+        if (cardData.m_Building && cardData.m_Building.CanBuildBuilding(map))
         {
             DestroyBuilding();
             building = cardData.m_Building;
             Vector3 newPosition = transform.position + building.transform.position;
-            var newRotation= building.transform.rotation * transform.rotation;
+            var newRotation = building.transform.rotation * transform.rotation;
             building = Instantiate(building, newPosition, newRotation, transform);
             building.m_CardData = cardData;
             building.slot = this;
         }
     }
-    
+
     public void DestroyBuilding()
     {
-        if(building != null)
+        if (building != null)
         {
             building.DestroyBuilding.Invoke();
             building.m_CardData = null;
@@ -61,12 +64,12 @@ public class BuildingSlot : MonoBehaviour
 
     private void OnMouseUp()
     {
-            Debug.Log("BuildCard 3");
+        Debug.Log("BuildCard 3");
 
 
-            EventManager.instance.BuildCard.Invoke(index);
+        EventManager.instance.BuildCard.Invoke(index);
     }
-    
+
     public CardImpact GetCardCalaulation(StatsManager statsManager, WorldMap map)
     {
         return building?.GetCardCalaulation(statsManager, map);
@@ -74,29 +77,43 @@ public class BuildingSlot : MonoBehaviour
 
     void OnMouseOver()
     {
-        if(StatsManager.Instance.CardIsDragged)
+        if (StatsManager.Instance.CardIsDragged)
         {
             StatsManager.Instance.buildingSlotSelected = this;
-           animator.SetBool("Hover", true);
+            animator.SetBool("Hover", true);
         }
-        if(building != null)
+        if (building != null && showingResources == null)
         {
-            resourcesPanel.SetActive(true);
+            IsHovering = true;
+           showingResources = StartCoroutine(ShowResourcesPanel());
         }
     }
+       
 
+     IEnumerator ShowResourcesPanel()
+    {
+        yield  return new WaitForSeconds(0.6f);
+        resourcesPanel.SetActive(IsHovering);
+
+    }
     void OnMouseExit()
     {
-           animator.SetBool("Hover", false);
-
-        if(StatsManager.Instance.CardIsDragged)
+        animator.SetBool("Hover", false);
+        IsHovering = false;
+        if (StatsManager.Instance.CardIsDragged)
         {
         }
-        if(StatsManager.Instance.buildingSlotSelected == this)
+        if (StatsManager.Instance.buildingSlotSelected == this)
         {
-            StatsManager.Instance.buildingSlotSelected =null;
+            StatsManager.Instance.buildingSlotSelected = null;
         }
         resourcesPanel.SetActive(false);
+        if (building != null && showingResources != null)
+        {
+            StopCoroutine(showingResources);
+            showingResources = null;
+        }
+
     }
 
 }
