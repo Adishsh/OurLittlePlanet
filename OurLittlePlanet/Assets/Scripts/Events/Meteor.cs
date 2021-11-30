d08293fee03d60cb8e35eea2811a6dbe4003e585
Adishsh <Adishsh@users.noreply.github.com>
2021-11-30 20:15:24 +0200

    meteor delay destroy

--- OurLittlePlanet/Assets/Scripts/Events/Meteor.cs
+++ OurLittlePlanet/Assets/Scripts/Events/Meteor.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,8 +8,7 @@ public class Meteor : EventCard
     
     public override void ActivateEvent(WorldMap map, StatsManager statsManager)
     {
-        var slotsToDestroy = map.GetRandom3x3();
-        map.DestroyBuildings(slotsToDestroy);
+        StartCoroutine(DelayedAction(()=>DestroyBuildings(map),1.5f));
         statsManager.SetCurrentEvent(this);
         
         if(m_Animator != null)
@@ -16,5 +16,16 @@ public class Meteor : EventCard
             m_Animator.SetTrigger("Go");
             //m_Animator.ResetTrigger("End");
         }
+    }
+    private void DestroyBuildings(WorldMap map)
+    {
+        var slotsToDestroy = map.GetRandom3x3();
+        map.DestroyBuildings(slotsToDestroy);
+    }
+    
+    IEnumerator DelayedAction(Action action, float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        action.Invoke();
     }
 }
